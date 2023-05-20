1458a572167012527735bd62e7dd23d2e66a2c3f
Michael Barcellini <michael.barcellini@gmail.com>
2023-05-20 22:32:39 +0200

    Exercice improved

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,10 @@
 class ReviewsController < ApplicationController
+  before_action :restaurant_id, only: %i[new create]
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
@@ -16,6 +15,10 @@ class ReviewsController < ApplicationController
   end
 
   private
+
+  def restaurant_id
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 
   def review_params
     params.require(:review).permit(:rating, :content)
