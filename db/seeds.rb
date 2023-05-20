4643a2d2437f1245487d8bf32b00c0e8b2d21c53
Michael Barcellini <michael.barcellini@gmail.com>
2023-05-20 22:16:49 +0200

    Exercice done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: %w[chinese italian japanese french belgian][rand(4)]
+  )
+  restaurant.save!
+  5.times do
+    review = Review.new(
+      restaurant_id: restaurant.id,
+      rating: rand(5),
+      content: Faker::Restaurant.review
+    )
+    review.save!
+  end
+end
