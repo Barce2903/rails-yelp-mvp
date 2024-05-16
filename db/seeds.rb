40b0dd0dd257cd1a922e473e401308ea1f70eabb
Michael Barcellini <michael.barcellini@gmail.com>
2024-05-16 12:56:40 +0200

    Seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+5.times do
+  restau = Restaurant.new(
+    name: 'Restaurant_test',
+    address: 'Chemin du test 43',
+    phone_number: '022 432 13 45',
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  restau.save
+  5.times do
+    rev = Review.new(
+      restaurant_id: restau.id,
+      rating: (0..5).to_a.sample,
+      content: 'Un restaurant comme un autre'
+    )
+    rev.save
+  end
+end
